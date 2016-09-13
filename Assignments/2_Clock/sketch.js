63f6031c314ef38c0d2dcd6aec7c0b5e027db37a
Kaleb <kaleb.crawford@gmail.com>
2016-09-13 07:22:46 -0400

    Demonic growth circle clock
    mostly done: todo list includes rotating the seconds so it actually
    lines up (does that matter if they are evenly distributed? and something
    else i forget

--- Assignments/2_Clock/sketch.js
+++ Assignments/2_Clock/sketch.js
@@ -1,34 +1,44 @@
-var w = 800;
-var h = 800;
+var w = 200;
+var h = 200;
 var margin = w/8;
 
 var numParticles = 100;
-var pStep = w/100;
-var di = w/100; //diameter of particles
+var pStep = w/200;
+var di = w/200; //diameter of particles
 var particleArray = [];
+var orbArray = [];
 var millisRolloverTime;
 var myTree;
 
-var sec, prevSec;
+var sec, prevSec, subsec, cHour;
 
 /////////////////////////////////SETUP/////////////////////////
 
 function setup() {
-
-  frameRate(100);
+  rectMode(CENTER);
+  frameRate(60);
   createCanvas(w, h);
   myTree = new Tree(width / 2, height / 2, 0);
   sec = second();
   prevSec = sec;
   millisRollovertime = 0;
   
+  //colors
+  tFill = color(30);
+  oFill = color(30);
+  
   //initialize with current minutes particles
-  for (var i = 0; i < minute(); i++) {
-    p = new Particle(random(width/8*3,width/8*5),random(height/8*3,height/8*5));
+  for (var i = 0; i < minute()*60; i++) {
+    p = new Particle(random(margin,width-margin),random(margin,height-margin));
     particleArray.push(p);
   }
   println("initialized with "+particleArray.length);
-
+  
+  //make orbs for hours
+  for (var j = 0; j < 23; j++) {
+    o = new Orb(width/2,height/2,j);
+    orbArray.push(o);
+  }
 
 }
 /////////////////////////////////DRAW//////////////////////////
@@ -36,15 +46,20 @@ function draw() {
   // background(200);
   noStroke();
 
-  var subSec = floor(millis() - millisRolloverTime) / 1000.0;
+  subSec = floor(millis() - millisRolloverTime) / 1000.0;
 
+  //Calculation for where 'front' of orbs is, which is where particles are added
   var x = map( cos( map( sec+subSec,0,60,0,2*PI) ),-1,1,margin,width-margin );
   var y = map( sin( map( sec+subSec,0,60,0,2*PI) ),-1,1,margin,height-margin );
   
+  for (var o = 0; o < orbArray.length; o++) {
+    orbArray[o].update(); //update first
+  }
+  
   sec = second(); //this MUST come after x y calculations
+  cHour = hour();
   
-  
-  //Time Stuff
+  //Add a new particle every "tick" of a second
   if (prevSec != sec) {
     millisRolloverTime = millis();
     prevSec = sec;
@@ -53,27 +68,25 @@ function draw() {
     println(hour()+':'+minute()+':'+sec);
   }
 
+  //Iterate over particle array, moving them, checking for collision with tree, then drawing them
   for (var i = 0; i < particleArray.length; i++) {
     particleArray[i].update();
     particleArray[i].collide(myTree);
-    particleArray[i].display();
     if (particleArray[i].collided === true) {
       println("collided");
       myTree.nodes.push(createVector(particleArray[i].pos.x, particleArray[i].pos.y));
       particleArray.splice(i, 1);
     }
+    particleArray[i].display();
   }
   
-  
-
-  
+  //Render nodes of the DLA tree
   myTree.display();
-
   
-  //draw indicator circle
-  fill(0);
-  ellipse(x,y,di*8,di*8);
-
+  //Render number of orbs based on hour
+  for (var o = 0; o < hour(); o++) {
+    orbArray[o].display(); //update first
+  }
 } //end of draw
 
 
@@ -82,34 +95,26 @@ function Particle(x, y) {
   this.pos = createVector(x, y);
   this.diameter = di;
   this.collided = false;
-  // this.acc = acc;
 
   ///////////////////////////
   this.update = function() {
-
+    
+    //drunk walk particles
     this.pos.x += random(-1, 1) * pStep;
     this.pos.y += random(-1, 1) * pStep;
 
-    //check bounds
-    if (this.pos.x <= 0) {
-      this.pos.x = 0
-    }
-    if (this.pos.x >= width) {
-      this.pos.x = width
-    }
-    if (this.pos.y <= 0) {
-      this.pos.y = 0
-    }
-    if (this.pos.y >= height) {
-      this.pos.y = height
-    }
+    //check bounds of particles against canvas
+    if (this.pos.x <= 0) { this.pos.x = 0 }
+    if (this.pos.x >= width) { this.pos.x = width }
+    if (this.pos.y <= 0) { this.pos.y = 0 }
+    if (this.pos.y >= height) { this.pos.y = height }
 
-  }; //end update method
+  }; //end particle update method
 
   ////////////////////////////
   this.display = function() {
       fill(255);
-      ellipse(this.pos.x, this.pos.y, this.diameter, this.diameter);
+      rect(this.pos.x, this.pos.y, this.diameter, this.diameter);
     } //end particle display method
 
   ////////////////////////////////		
@@ -120,7 +125,6 @@ function Particle(x, y) {
         } //end distance check
       } //end for loop
     } //end particle collide method
-
 } //end of particle object
 
 ///////////////////////////////////////////////Tree Object////////////////////////////////////
@@ -131,30 +135,29 @@ function Tree(startX, startY, rot) {
   ///////////////////////////
   this.display = function() {
       for (var i = 0; i < this.nodes.length; i++) {
-        // this.nodes[i] = rotateAngle(random(100,150),random(100,150),this.nodes[i].x, this.nodes[i].y,1);
-        fill(0, 0, 0);
-        ellipse(this.nodes[i].x, this.nodes[i].y, this.diameter, this.diameter);
+        fill(tFill);
+        rect(this.nodes[i].x, this.nodes[i].y, this.diameter, this.diameter);
       } //end for loop
     } //end tree display method
 } //end of tree object
 
+///////////////////////////////////////////////Orb Object////////////////////////////////////
+function Orb(x,y,id) {
+  this.pos = [createVector(x, y)];
+  this.diameter = di*12;
+  this.id = id;
+  
+  this.update = function() {
+    //calculate the position of each orb using unit circle rotation over minute, divided by how many orbs
+    this.pos.x =  map( cos( map( sec+subSec,0,60,0,2*PI)-radians(this.id*360/cHour)),-1,1,margin,width-margin );
+    this.pos.y =  map( sin( map( sec+subSec,0,60,0,2*PI)-radians(this.id*360/cHour)),-1,1,margin,height-margin );
+  }//end orb update method
 
-function rotateAngle (pivotX,pivotY,pointX,pointY,angle) {
-
-        var sine = sin(radians(angle));
-        var cosine = cos(radians(angle));
-        
-        pointX = map(pointX - pivotX,0,width,0,1);
-        pointY = map(pointY - pivotY,0,height,0,1);
-
-        var xNew = pointX * cosine - pointY * sine;
-        var yNew = pointX * sine - pointY * cosine;
-        
-        
-        var newPoint = createVector (xNew + pivotX,yNew + pivotY);
-        return newPoint;
-    } //end rotateAngle
-
-
+  ///////////////////////////
+  this.display = function() {
+      fill(oFill);
+      ellipse(this.pos.x, this.pos.y, this.diameter, this.diameter);
+    } //end orb display method
+} //end of orb object
 
 //////
