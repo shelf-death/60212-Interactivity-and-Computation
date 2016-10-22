ac24fa19c73b1b85617061a8d11a14fe6eed8b73
Kaleb <kaleb.crawford@gmail.com>
2016-10-22 02:13:28 -0400

    nlp-compromise migration, wordnet downloaded
    compromise under sketch.js, rita kept as duplicate. negate, reconjugate,
    and pluralize working

--- Assignments/6_GenBook/sketch.js
+++ Assignments/6_GenBook/sketch.js
@@ -5,16 +5,18 @@
 var wordCount = 0;	//total wordcount
 var sText; 			//Source Text
 var joinedText;		//text array joined into single string
-var words;			//tokenized words
+var oneLine;		//nlp object
 var lineArray = [];	//Array of all lines of text
+
+//NlP Compromise
+var nlp = window.nlp_compromise;
 
 //for keeping references to index for swapping
 var randLine,randWord,firstWord;
 
 /////////////////////////Preload/////////////////////
 function preload() {
-	sText = loadStrings('texts/stevens.txt');
-	lexicon = new RiLexicon();
+	sText = loadStrings('texts/Stevens.txt');
 }//end preload
 
 //////////////////////////Setup//////////////////////
@@ -23,62 +25,54 @@ function setup() {
 	background(230);
 
 	for (var i = 0; i < sText.length; i++){
-		words = RiTa.tokenize(sText[i],' ');
-		lineArray[i] = words;
+		oneLine = nlp.sentence(sText[i]);
 
-		wordCount += words.length;
+    negateLine(sText[i]);
+		
+		lineArray[i] = oneLine;
+
 	}//end for loop
 
-	console.log(wordCount);
-	console.log(lineArray);
-
-	getSimilarSound(wordCount*2);
-	// getRhymes(wordCount/3);
+	// getSimilarSound(10);
+	// getRhymes(50);
 	reBuildText();
 	
 }//end setup
 
-///////////////////Get Similar Sound//////////////////
-function getSimilarSound(num) {
-	for (var i = 0; i < num; i++){
-		randLine = int(random(lineArray.length));
-		randWord = int(random(lineArray[randLine].length));
-		firstWord = lineArray[randLine][randWord];
+///////////////////Reconjugate verbs//////////////////
+function negateLine(theLine) {
+  oneLine = nlp.statement(theLine).negate();
+}
 
-		var similarSound = lexicon.similarBySound(firstWord);
+///////////////////Reconjugate verbs//////////////////
+function reconjugate() {
+  for (var t = 0; t < oneLine.terms.length; t++) {
+			var part = oneLine.terms[t].pos;
+			var oneTerm = oneLine.terms[t];
+			if (part.Verb){
+				oneLine.terms[t].text = 
+				nlp.verb(
+				  oneTerm.text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+				  ).conjugate().present;
+			}
+		}//end for loop
+}//endpluralize line
 
-		if (similarSound.length > 0){
-		lineArray[randLine][randWord] = similarSound[0];
-		console.log(firstWord);
-		console.log(similarSound[0]);
-		}//end if
-		else{i-=1}
-	}//end num loop
-}//end Similar Sound function
-
-///////////////////Get Rhymes//////////////////
-function getRhymes(num) {
-	for (var i = 0; i < num; i++){
-		randLine = int(random(lineArray.length));
-		randWord = int(random(lineArray[randLine].length));
-		firstWord = lineArray[randLine][randWord];
-
-		var rhymes = lexicon.rhymes(firstWord);
-
-		if (rhymes.length > 0){
-		lineArray[randLine][randWord] = rhymes[0];
-		console.log(firstWord);
-		console.log(rhymes[0]);
-		}//end if
-		else{i-=1}
-	}//end num loop
-}//end Similar Sound function
+///////////////////Pluralize Line//////////////////
+function pluralizeLine() {
+  for (var t = 0; t < oneLine.terms.length; t++) {
+			var part = oneLine.terms[t].pos;
+			var oneTerm = oneLine.terms[t];
+			if (part.Noun && !part.Pronoun){
+				oneLine.terms[t].text = nlp.noun(oneTerm.text).pluralize();
+			}
+		}//end for loop
+}//endpluralize line
 
 ///////////////////Rebuild Text//////////////////
 function reBuildText() {
 	for (var i = 0; i < lineArray.length; i++){
-		lineArray[i] = lineArray[i].join(' ');
-		console.log(lineArray[i]);
-		text(lineArray[i], 10,i*15+30,500,500);
+		console.log(lineArray[i].str);
+		text(lineArray[i].text(), 10,i*15+30,500,500);
 	}
 }//end rebuild text
