85cf715f7c2972213c4dbd24e751cae1193a99ef
kupodude <kaleb.crawford@gmail.com>
2016-11-16 22:54:23 -0500

    Songs and its lit

--- Assignments/9_NetworkedObject/sketch.js
+++ Assignments/9_NetworkedObject/sketch.js
@@ -1,4 +1,4 @@
-var eu = [1,1];	 																									//Euclidian Rhythm
+var eu = [6,10];																										//Euclidian Rhythm
 var d = Math.floor(eu[1]/eu[0]);  								//Divisor
 var r = eu[1]%eu[0];  																				//Remainder
 if (eu[0] > (eu[1]/2)) { r = eu[1]-eu[0]}	//If the value doesn't divide once, remainder is difference
@@ -7,31 +7,49 @@ var shift = 0;
 
 var mySound, myPhrase, myPart;
 
-var freq =  				//relative to c3 = 1
-[0.8409143032,		//a
-0.943888082,				//b
-1.000000000,				//c
-1.122467701,				//d
-1.259918966,			 //e
-1.334836786,				//f
-1.498356395]; 		//gN
+var notes =  ["c","d","e","f","g","a","b"];
+var noteScale = [72, 74, 76, 77, 79, 81, 83]; // 48 is base note
+var currentNote = 0;
+var currentNote2 = 0;
+
 
 var list = []; 
-var s = "sick beats bro";
-var notes = ["a","b","c","d","e","f","g"];
-
+var s = "golan levin is lit af af af studio of creative inquiry"
 ///////////////////////////////////Preload/////////////////////////////////////////
 function preload() {
-  mySound = loadSound('assets/beatbox.mp3');
+  
 }
 ///////////////////////////////////Setup/////////////////////////////////////////
 function setup() {
 	
-	masterVolume(0.1);
-	myPhrase = new p5.Phrase('bbox', makeSound, list);
- myPart = new p5.Part();
- myPart.addPhrase(myPhrase);
- myPart.setBPM(60);
+	//Web MIDI Object Check
+	// request MIDI access
+	if (navigator.requestMIDIAccess) {
+	    navigator.requestMIDIAccess({
+	        sysex: false // this defaults to 'false' and we won't be covering sysex in this article. 
+	    }).then(onMIDISuccess, onMIDIFailure);
+	} else {
+	    // alert("No MIDI support in your browser.");
+	}
+	
+	// midi functions
+	function onMIDISuccess(midiAccess) {
+	    // when we get a succesful response, run this code
+	    console.log('MIDI Access Object', midiAccess);
+	}
+	
+	function onMIDIFailure(e) {
+	    // when we get a failed response, run this code
+	    console.log("No access to MIDI devices or your browser doesn't support WebMIDI API. Please use WebMIDIAPIShim " + e);
+	}
+	////////end of MIDI shit
+	
+	osc = new p5.Oscillator('triangle');
+	// osc.start();
+	frameRate(8);
+	
+	osc2 = new p5.Oscillator('sine');
+	osc2.start();
 	
 	calculateEuclid(eu,beat,shift);
 	textToBeat(s,notes,list);
@@ -43,16 +61,21 @@ function setup() {
 ///////////////////////////////////Draw///////////////////////////////////////////
 function draw() {
   background(0);
-}//end draw
-
-
-function mouseClicked() {
-  if (1) {
-    myPart.start();
-    msg = 'playing pattern';
+  
+  freq = midiToFreq(list[currentNote]);
+  freq2 = midiToFreq(beat[currentNote2])
+  osc.freq(freq);
+  osc2.freq(freq2);
+  currentNote++;
+  currentNote2++;
+  if (currentNote >= list.length){
+    currentNote = 0;
   }
-}
-
+  if (currentNote2 >= beat.length){
+    currentNote2 = 0;
+  }
+  
+}//end draw
 ///////////////////////////////////makeSound///////////////////////////////////////////
 function makeSound(time, playbackRate) {
   mySound.rate(playbackRate);
@@ -64,11 +87,11 @@ function textToBeat(words,noteList,melody) {
 	for (var i = 0; i<words.length; i++){
     for (var j = 0; j<noteList.length; j++){
       if (words[i] == noteList[j]){
-        melody.push(freq[j]);
+        melody.push(noteScale[j]);
       }
     }
     if (melody[i] != 1){
-      melody.push(0);    
+      melody.push(melody[i-1]); //base note    
     }
   }
   console.log(melody);
@@ -77,7 +100,7 @@ function textToBeat(words,noteList,melody) {
 function calculateEuclid(euclid,beatArray,shiftAmt) {
   for (var i = 0; i < euclid[0]; i++){
   	
-  	beatArray.push(1); 																									//initialize with beats
+  	beatArray.push(60); 																									//initialize with beats
   	
   	if (d > 1) {
 	  	for (var j = 0; j < d-1; j++ ){
