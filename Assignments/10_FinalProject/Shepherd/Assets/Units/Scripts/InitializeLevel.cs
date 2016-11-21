0c2dd8973000f39534637421dcb411bab8f5f702
kupodude <kaleb.crawford@gmail.com>
2016-11-20 22:29:54 -0500

    Progress
    units seek the shepherd, but kinematics on shepherd cause them to not
    move

---
+++ Assignments/10_FinalProject/Shepherd/Assets/Units/Scripts/InitializeLevel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitializeLevel : MonoBehaviour {
+
+	public Transform vassal;
+	public float xSpacing = 3.0f;
+	public float zSpacing = 3.0f;
+
+	// Use this for initialization
+	void Start() {
+		for (int z = 0; z < 10; z++) {
+			for (int x = 0; x < 10; x++) {
+				Instantiate(vassal, new Vector3(x*xSpacing, 0, z*zSpacing), Quaternion.identity);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
